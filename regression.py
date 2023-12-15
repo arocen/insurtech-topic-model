146b47b2a33a77be12f59c4b94bd01bbdb40c61a
arocen <aufger1913@hotmail.com>
2023-12-15 13:13:43 +0800

    loop and do regressions to each company, the result is not significant

--- regression.py
+++ regression.py
@@ -15,15 +15,8 @@ index_df = pd.read_excel(InsurTech_path, "rescale", index_col=0)
 complaints_path = os.environ.get("complaints")
 complaints_df = pd.read_excel(complaints_path, "总投诉量", index_col=0)
 
-# # 平安
-# # Add a constant term for intercept
-# X_pingan = sm.add_constant(index_df.loc['平安'])
-
-# # Fit the linear regression model
-# model = sm.OLS(complaints_df.loc['平安'], X_pingan).fit()
-
-# # Print the regression results
-# print(model.summary())
-
-# for rowlabel in ["平安"“]
-print(list(index_df.index))
+# Loop and do regressions
+for company in list(index_df.index):
+    X = sm.add_constant(index_df.loc[company])
+    model = sm.OLS(complaints_df.loc[company], X).fit()
+    print(model.summary())
