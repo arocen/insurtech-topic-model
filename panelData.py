76cb1c80765401c7f8d8469d624851ceb5519317
arocen <aufger1913@hotmail.com>
2023-12-19 19:21:43 +0800

    finished panel data model

--- panelData.py
+++ panelData.py
@@ -1,2 +1,30 @@
 # Run regressions on panel data.
 
+import pandas as pd
+from linearmodels import PanelOLS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def runModel(input_df:pd.DataFrame):
+    '''Run a fixed effects regression'''
+
+    # Set index
+    panel_data = input_df.set_index(['company', 'year'])
+
+    # Run a fixed effects regression
+    model = PanelOLS(panel_data['总投诉量'], panel_data[['保险科技指标', '原保费收入']], entity_effects=True)
+    results = model.fit()
+
+    # Display regression results
+    print(results)
+
+    return
+
+# Load panel data from Excel
+input_variables_path = os.environ.get("input_variables")
+inputEst_df = pd.read_excel(input_variables_path, "input_est", skiprows=[0, 1, 2])
+print(inputEst_df)
+# runModel(inputEst_df)
